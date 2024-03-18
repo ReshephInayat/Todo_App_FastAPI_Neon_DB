9b89141432abc116adfbaf21313e15f151f4123a
Resheph Inayat <rasafinayat@gmail.com>
2024-03-18 10:24:27 +0500

    Initial Commit

---
+++ todo-app/app/main.py
@@ -0,0 +1,123 @@
+# Here We are Importing fastapi From FastAPI
+# Dependencies: It is just a function that can take all the same parameters that a path operation function can take
+# https://fastapi.tiangolo.com/tutorial/dependencies/
+from fastapi import FastAPI, Depends, HTTPException
+
+
+# If you check, the function is decorated with an @asynccontextmanager.
+# That converts the function into something called an "async context manager"
+# A context manager in Python is something that you can use in a with statement, for example, open() can be used as a context manager:
+from contextlib import asynccontextmanager
+
+
+# Importing sqlmodel from SQlmodel: SQLModel is based on Python type annotations, and powered by Pydantic and SQLAlchemy
+# Field: https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/# define-the-fields-columns
+# Session:
+# select:
+# create_engine: It is an object that handles the communication with the database.If you have a server database (for example PostgreSQL or MySQL), the engine will hold the network connections to that database.
+from sqlmodel import SQLModel, Field, Session, select, create_engine
+
+
+# importing Optional Type from typing python typing package
+# Annotated: https://fastapi.tiangolo.com/tutorial/dependencies/#share-annotated-dependencies
+from typing import Annotated, Optional
+
+
+# importing setting modules
+from app import settings
+
+app: FastAPI = FastAPI()
+
+# The name of each of these variables will be the name of the column in the table.      And the type of each of them will also be the type of table column:
+
+# primary_key:Now let's review the id field. This is the primary key of the table.So, we need to mark id as the primary key.To do that, we use the special Field function from sqlmodel and set the argument primary_key=True:
+# https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#primary-key-id
+
+
+class Todo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    content: str = Field(index=True)
+
+
+# only needed for psycopg 3
+# Replacing postgresql with postgresql+psycopg2
+connection_string = str(settings.DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg"
+)
+
+
+# The pool_recycle=300 option is an “optimistic” approach to prevent the pool from using a connection that has passed a certain age. In this case, we are setting the value to 5 minutes to correspond with the default compute auto-suspend in Neon.
+# https://docs.sqlalchemy.org/en/20/core/pooling.html#setting-pool-recycle
+engine = create_engine(
+    connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
+)
+# Creating the engine is very simple, just call create_engine() with a URL for the database to use
+# in this case we stored the database url in a variable called connection_string and passed it as an argument
+# https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#create-the-engine
+
+
+# https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#sqlmodel-metadata
+# https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#calling-create_all
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+# The first part of the function, before the yield, will
+# be executed before the application starts.
+# https://fastapi.tiangolo.com/advanced/events/#lifespan-function
+@asynccontextmanager
+# A context manager in Python is something that you can use in a with statement
+async def lifespan(app: FastAPI):
+    print("Creating tables..")
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+# with Session(engine) as session: This establishes a database session using some database engine (probably SQLAlchemy) defined in engine. The with statement ensures proper session management, automatically closing the session when the block exits.
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/todos/", response_model=Todo)
+def create_todo(todo: Todo, session: Annotated[Session, Depends(get_session)]):
+    # session.add(todo): This adds the provided todo object to the session. This action stages the object for insertion into the database.
+    session.add(todo)
+    # session.commit(): This commits the transaction to the database, effectively persisting the changes (adding the new todo) to the database.
+    session.commit()
+    # session.refresh(todo): This refreshes the state of the todo object from the database. This might be necessary if there are any database triggers or defaults that are applied upon insertion.
+    session.refresh(todo)
+    return todo
+
+
+@app.get("/todos/", response_model=list[Todo])
+def read_todos(session: Annotated[Session, Depends(get_session)]):
+    todos = session.exec(select(Todo)).all()
+    return todos
+
+
+# with Session(engine) as session: Similar to the create_todo function, this establishes a database session using the provided database engine.
+
+# - todos = session.exec(select(Todo)).all(): This executes a SELECT query on the Todo table (assuming Todo is a SQLAlchemy model) and retrieves all todo items from the database. It uses the select construct to create a SELECT statement, and all() fetches all results.
+
+# return todos: Finally, the retrieved todo items are returned as the response to the client. This is typically returned as a JSON array.
+
+
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int, session: Annotated[Session, Depends(get_session)]):
+    # Fetch the Todo item to be deleted
+    todo = session.get(Todo, todo_id)
+    if todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    # Delete the Todo item from the database
+    session.delete(todo)
+    session.commit()
+    return {"message": "Todo deleted successfully"}
