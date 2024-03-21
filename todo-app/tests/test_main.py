a6ebe38253872b9f756c7401087fcbba5f33a24a
Resheph Inayat <rasafinayat@gmail.com>
2024-03-22 01:37:37 +0500

    Pytest Error Resolved

--- todo-app/tests/test_main.py
+++ todo-app/tests/test_main.py
@@ -5,12 +5,12 @@
 
 
 from fastapi.testclient import TestClient
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-from app.main import app, get_session, Todo
+from sqlmodel import Session, SQLModel, create_engine
+from app.main import app, get_session
 from app import settings
 
 
-
+# ===================================================================================================
 def test_read_main():
     client = TestClient(app=app)
     response = client.get("/")
@@ -18,13 +18,26 @@ def test_read_main():
     assert response.json() == {"Response": "Todo App"}
 
 
+def test_read_main_2():
+    client = TestClient(app=app)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() != {"message": "Hello World"}
+
+
+# ===================================================================================================
+
+
+# ===================================================================================================
 def test_create_todo():
+
     connection_string = str(settings.TEST_DATABASE_URL).replace(
         "postgresql", "postgresql+psycopg"
     )
     engine = create_engine(
         connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
     )
+
     SQLModel.metadata.create_all(engine)
 
     with Session(engine) as session:
@@ -35,24 +48,51 @@ def test_create_todo():
         app.dependency_overrides[get_session] = get_session_override
 
         client = TestClient(app=app)
-        todo_content = "buy bread"
+        todo_content = "buy Milky Bread"
         response = client.post("/todos/", json={"content": todo_content})
         data = response.json()
         assert response.status_code == 200
         assert data["content"] == todo_content
 
 
-def test_read_todos():
+def test_create_todo_2():
+
     connection_string = str(settings.TEST_DATABASE_URL).replace(
         "postgresql", "postgresql+psycopg"
     )
     engine = create_engine(
         connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
     )
+
     SQLModel.metadata.create_all(engine)
 
     with Session(engine) as session:
 
+        def get_session_override():
+            return session
+
+        app.dependency_overrides[get_session] = get_session_override
+
+        client = TestClient(app=app)
+        todo_content = 1
+        response = client.post("/todos/", json={"content": todo_content})
+        data = response.json()
+        assert response.status_code == 200
+        assert data["content"] != todo_content
+
+
+# ===================================================================================================
+
+
+# ===================================================================================================
+def test_read_todos():
+
+    connection_string = str(settings.TEST_DATABASE_URL).replace("postgresql", "postgresql+psycopg")
+    engine = create_engine(connection_string, connect_args={"sslmode": "require"}, pool_recycle=300)
+
+    SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
         def get_session_override():
             return session
 
@@ -62,6 +102,30 @@ def test_read_todos():
         response = client.get("/todos/")
         assert response.status_code == 200
 
+#                   W o r ki n g      on    This
+
+# def test_read_todos_2():
+#     connection_string = str(settings.TEST_DATABASE_URL).replace(
+#         "postgresql", "postgresql+psycopg"
+#     )
+#     engine = create_engine(
+#         connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
+#     )
+#     SQLModel.metadata.create_all(engine)
+#     with Session(engine) as session:
+#         def get_session_override():
+#             return session
+#         app.dependency_overrides[get_session] = get_session_override
+#         client = TestClient(app=app)
+#         result = {"content": "Buy groceries"}
+#         response = client.get("/todos/", json={"content": result["content"]})
+#         data = response.json()
+#         assert response.status_code == 200
+#         # assert data["id"] == JSON.id
+#         assert data["content"] == result.content
+
+# # ===================================================================================================
+
 
 # Test Get Single Todo Pending.......
 
@@ -70,6 +134,3 @@ def test_read_todos():
 
 
 # Test Update Todo Pending.......
-
-
-
